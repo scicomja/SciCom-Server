80708c8c4210c3d3b2e592843819873e6d6047bf
Travis <ga86yam@mytum.de>
2019-07-01 00:06:17 +0200

    add master thesis as project type

--- constants.js
+++ constants.js
@@ -1,51 +1,41 @@
 const germanStates = [
-  "Bayern",
-  "Berlin",
-  "Niedersachsen",
-  "Baden-Württemberg",
-  "Rheinland-Pfalz",
-  "Sachsen",
-  "Thüringen",
-  "Hessen",
-  "Nordrhein-Westfalen",
-  "Sachsen-Anhalt",
-  "Brandenburg",
-  "Mecklenburg-Vorpommern",
-  "Hamburg",
-  "Schleswig-Holstein",
-  "Saarland",
-  "Bremen"
+	"Bayern",
+	"Berlin",
+	"Niedersachsen",
+	"Baden-Württemberg",
+	"Rheinland-Pfalz",
+	"Sachsen",
+	"Thüringen",
+	"Hessen",
+	"Nordrhein-Westfalen",
+	"Sachsen-Anhalt",
+	"Brandenburg",
+	"Mecklenburg-Vorpommern",
+	"Hamburg",
+	"Schleswig-Holstein",
+	"Saarland",
+	"Bremen"
 ]
 
-const projectStatus = [
-  "open",
-  "active",
-  "completed",
-  "closed",
-  "deleted"
-]
+const projectStatus = ["open", "active", "completed", "closed", "deleted"]
 
 const projectType = [
-  'internship',
-  'thesis',
-  'parttime',
-  'voluntary',
-  'quick-question'
+	"internship",
+	"thesis",
+	"parttime",
+	"voluntary",
+	"quick-question",
+	"masterThesis"
 ]
-const applicationStatus = [
-  "pending",
-  "accepted",
-  "rejected"
-]
+const applicationStatus = ["pending", "accepted", "rejected"]
 
-const projectDir = 'projects/'
-const userUploadDir = 'uploads/'
+const projectDir = "projects/"
+const userUploadDir = "uploads/"
 module.exports = {
-  germanStates,
-  projectStatus,
-  projectType,
-  projectDir,
-  userUploadDir,
-  applicationStatus
-
+	germanStates,
+	projectStatus,
+	projectType,
+	projectDir,
+	userUploadDir,
+	applicationStatus
 }
